2aa5e7871058da0c2f8a0124e38997a72888a399
aematei <alexanderematei@gmail.com>
2022-12-03 11:11:01 -0800

    Fix docstrings and implement type hinting and argument defaults

--- controller/lighting_unit.py
+++ controller/lighting_unit.py
@@ -3,138 +3,128 @@ from machine import Pin
 
 
 class LightingUnit:
-    """A class representing an LED lighting unit. It implements some basic
-    functionality necessary for controlling a hardware LED lighting unit,
-    such as those made up of addressable pixels (like the WS2812B) connected
-    in serial
-
-    :param outputPin: the pin ID for the controller's output to the lighting
-        unit
-    :type outputPin: int
-    :param pixelCount: the number of pixels in the lighting unit
-    :type pixelCount: int
     """
-    def __init__(self, outputPin, pixelCount):
-        """Constructor method
+    A class representing an LED lighting unit.
+    """
+
+    def __init__(self, outputPin: int = 1, pixelCount: int = 0):
+        """
+        Constructor method for creating a LightingUnit object.
+
+        :param outputPin: controller's output pin ID to unit
+        :param pixelCount: the number of pixels in the unit
         """
         self._pinOut = Pin(outputPin, Pin.OUT)
-        self._pixelCount = pixelCount # number of pixel in the lighting unit
+        self._pixelCount = pixelCount
         self._color = (0, 0, 0)
         self._brightness = 0
         self._lightingUnit = NeoPixel(self._pinOut, self._pixelCount)
 
 
     # Public Interface
-    def set(self, color, brightness):
-        """Sets the color and brightness of the lighting unit
+    def set(self, color: "tuple[int, int, int]", brightness: int):
+        """
+        Sets the color and brightness of the lighting unit.
 
-        :param color: RGB tuple representing the color of the lighting unit, 
-            with each value being between 0 and 255, inclusive
-        :type color: int tuple
-        :param brightness: the brightness of the lighting unit, between 0 and
-            100, inclusive
-        :type brightness: int
+        :param color: RGB representation of unit's color, 0-255 for each
+        :param brightness: the brightness of the unit, 0-100
         """
         self._setColor(color)
         self._setBrightness(brightness)
         self._updateUnit()
 
-    def setColor(self, color):
-        """Sets the color of the lighting unit
+    def setColor(self, color: "tuple[int, int, int]"):
+        """
+        Sets the color of the lighting unit.
         
-        :param color: RGB tuple representing the color of the lighting unit,
-            with each value being between 0 and 255, inclusive
-        :type color: int tuple
+        :param color: RGB representation of unit's color, 0-255 for each
         """
         self._setColor(color)
         self._updateUnit()
 
-    def setBrightness(self, brightness):
-        """Sets the brightness of the lighting unit
+    def setBrightness(self, brightness: int):
+        """
+        Sets the brightness of the lighting unit.
         
-        :param brightness: the brightness of the lighting unit, between 0 and
-            100, inclusive
-        :type brightness: int
+        :param brightness: the brightness of the unit, 0-100
         """
         self._setBrightness(brightness)
         self._updateUnit()
 
-    def setPinOut(self, outputPin, zeroFormerPin=True):
-        """Sets the output pin from the MCU to the lighting unit
+    def setPinOut(self, outputPin: int, zeroFormerPin: bool = True):
+        """
+        Sets the output pin from the MCU to the lighting unit.
 
-        :param outputPin: the pin ID for the controller's output to the lighting
-            unit
-        :type outputPin: int
+        :param outputPin: controller's output pin ID to unit
         """
         if zeroFormerPin: # Send message to former pinOut to turn all pixels off
             self.lightingUnit.fill((0, 0, 0))
         self._setPinOut(outputPin)
         self._updateUnit()
 
-    def setPixelCount(self, pixelCount):
-        """"Sets the pixel count of the LightingUnit object
+    def setPixelCount(self, pixelCount: int):
+        """"
+        Sets the pixel count of the unit.
         
-        :param pixelCount: the number of pixels in the lighting unit
-        :type pixelCount: int"""
+        :param pixelCount: the number of pixels in the unit
+        """
         self._setPixelCount(pixelCount)
         self._updateUnit()
 
 
     #Private Methods
-    def _setColor(self, color):
-        """Sets the color without updating the lighting unit
+    def _setColor(self, color: "tuple[int, int, int]"):
+        """
+        Sets the color without updating the unit.
 
-        :param color: RGB tuple representing the color of the lighting unit,
-            with each value being between 0 and 255, inclusive
-        :type color: int tuple
+        :param color: RGB representation of unit's color, 0-255 for each
         """
         self._color = color
-        #TODO: validate color
 
-    def _setBrightness(self, brightness):
-        """Sets brightness without updating the lighting unit
+    def _setBrightness(self, brightness: int):
+        """
+        Sets brightness without updating the unit.
         
-        :param brightness: the brightness of the lighting unit, between 0 and
-            100, inclusive
-        :type brightness: int
+        :param brightness: :param brightness: the brightness of the unit, 0-100
         """
         self._brightness = brightness
-        #TODO: validate brightness
 
-    def _setPinOut(self, pinOut):
-        """Sets the output pin
+    def _setPinOut(self, pinOut: int):
+        """Sets the output pin without updating the unit.
 
-        :param pinOut: the pin ID for the controller's output to the lighting
-            unit
-        :type pinOut: int
+        :param pinOut: controller's output pin ID to unit
         """
         self._pinOut = pinOut
-        #TODO: validate pinOut
 
-    def _setPixelCount(self, pixelCount):
-        """Sets the pixel count
+    def _setPixelCount(self, pixelCount: int):
+        """
+        Sets the pixel count without updating the unit.
 
-        :param pixelCount: the number of pixels in the lighting unit
-        :type pixelCount: int
+        :param pixelCount: the number of pixels in the unit
         """
         self._pixelCount = pixelCount
 
-    def _scaleColorByBrightness(self):
-        """Returns the unit's color tuple scaled to the unit's brightness
+    def _scaleColorByBrightness(self) -> "tuple[int, int, int]":
+        """
+        Returns the unit's color tuple scaled to the unit's brightness.
         
-        :return: unit's color tuple scaled to the unit's brightness
-        :rtype: color tuple of size 3 with value between 0 and 255, inclusive
+        :return: unit's RGB color tuple scaled by the unit's brightness
         """
         scaledColor = []
+
         for component in self._color:
             try:
-                scaledColor.append(int(round(
-                    component * self._brightness / 100)))
+                scaledColor.append(
+                    int(round(component * self._brightness / 100)))
             except ZeroDivisionError:
                 scaledColor.append(0)
-        #TODO: Verify scaledColor is of length 3
+
         return tuple(scaledColor)
 
     def _updateUnit(self):
+        """
+        Updates the unit's brightness and color state based on object's
+        attributes.
+        """
         self._lightingUnit.fill(self._scaleColorByBrightness())
         self._lightingUnit.write()
