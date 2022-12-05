a3ad0d16df1b8a119b1b5f10fd1ecc8f78e12b70
ALINZ22 <ALANMONGX@GMAIL.COM>
2022-12-05 02:18:30 -0800

    home page css + navbar component and others for future use

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,44 @@
-import React, {FC} from 'react';
+import React, { FC } from "react";
 import "./css/App.css";
-import { Login } from "./components/Login"
-import { Register } from "./components/Register"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom" 
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/Header";
+import SiteInfo from "./components/Main";
 
 const App: FC = () => {
   return (
-    <div className="App">
-
-      <Router>
-        <Routes>
-          <Route path='/' element={
-          <div>
-            <h1>Home Page</h1>
-            <h2><Link to="/login">Login</Link></h2>
-            <h2><Link to="/register">Register</Link></h2>
-          </div>
-        }/>
-          <Route path='/login' element={
-            <div className="Login">
-              <Login username='username' password='password'/>
-            </div>
-          }/>
-          <Route path='/register' element={
-            <div className="Register">
-              <Register username='username' password='password' />
-            </div>
-          }/>
-          <Route path='*' element={ <h1>404 Not Found</h1> } />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="App">
+        <SiteInfo />
+        <Router>
+          <Routes>
+            <Route path="/" element={<div></div>} />
+            <Route
+              path="/login"
+              element={
+                <div className="Login">
+                  <Login username="username" password="password" />
+                </div>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <div className="Register">
+                  <Register username="username" password="password" />
+                </div>
+              }
+            />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
