45cabdfcf45f5cbbe90a4ad787737d7da717e72d
pogi7 <aaronlevitt7@gmail.com>
2022-12-18 12:22:22 -0800

    capitalize 'username' in Register.tsx

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -28,7 +28,7 @@ export const Register: FC<Users> = (props: Users) => {
   return (
     <div className="register-cover">
         <h1>Register</h1>
-        <input type="username" placeholder="username" />
+        <input type="username" placeholder="Username" />
         {usePasswordVisibility()}
 
         <div className="sign-up-btn">Sign Up</div>
