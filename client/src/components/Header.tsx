417dd5fc076f84aa7314d9f70cddaec6f3772317
1510749 <piewithicecream@gmail.com>
2022-12-20 00:38:43 -0800

    reverted the +20px & replaced hyperlink with image

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,10 +3,8 @@ import React, {FC, ChangeEvent, useState} from 'react';
 export default function Navbar() {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">
-        Luminosity LED
-        <img src="/images/luminosity-led-logo.ico"></img>
-
+      <a href="/" className="site-title"> 
+          <img src="/images/luminosity-home-logo.ico" alt="Home-logo"></img>
       </a>
 
       <ul>
