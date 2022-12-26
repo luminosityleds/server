e6a58a9ac27916a88c576ae45f32fca352b246b4
pogi7 <aaronlevitt7@gmail.com>
2022-12-25 23:12:30 -0800

    HashRouter for GH pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'
-import App from './App';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router} from "react-router-dom"
 
- ReactDOM.createRoot(document.getElementById('root')).render(
-   <React.StrictMode>
-     <HashRouter>
-       <App />
-     </HashRouter>
-   </React.StrictMode>
- );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router> 
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
