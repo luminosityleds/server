c181eaefae4ed8d8d64f6f138eb33e6da64247b2
pogi7 <aaronlevitt7@gmail.com>
2022-12-31 17:15:15 -0800

    update size of issues and redundant version name

--- scripts/create_jira_release_notes.py
+++ scripts/create_jira_release_notes.py
@@ -9,13 +9,11 @@ jira = JIRA(options=JIRA_OPTIONS, basic_auth=(os.environ['JIRA_USERNAME'], os.en
 
 VERSION = jira.version(JIRA_PROJECT_ID)
 
-def write_version_fields():
-    """ Appends following version fields to ./release_notes.md:
-        name: str
+def write_version_description():
+    """ Appends version description field to ./release_notes.md:
         description: str """
     # 'a' = append mode
     with open('release_notes.md', 'a') as f:
-        f.write(f"<h1>{VERSION}</h1>\n\n")
         f.write(f"{VERSION.description}\n\n")
 
 def write_bug_fields():
@@ -28,7 +26,7 @@ def write_bug_fields():
     with open('release_notes.md', 'a') as f:
         f.write('<h1> Bug <img src="https://cdn-icons-png.flaticon.com/512/1320/1320452.png" alt="drawing" width="25"/> </h1>\n\n')
         for bug in bugs:
-            f.write(f"<h2><a href=https://luminosity-led.atlassian.net/browse/{bug.key}>{bug.key} </a>{bug.get_field('summary')}</h2>" + '\n\n')
+            f.write(f"<h3><a href=https://luminosity-led.atlassian.net/browse/{bug.key}>{bug.key} </a>{bug.get_field('summary')}</h3>" + '\n\n')
 
 def write_story_fields():
     """ Appends following story fields to ./release_notes.md:
@@ -40,9 +38,9 @@ def write_story_fields():
     with open('release_notes.md', 'a') as f:
         f.write('<h1> Story <img src="https://cdn-icons-png.flaticon.com/512/2421/2421066.png" alt="drawing" width="25"/> </h1>\n\n')
         for story in stories:
-            f.write(f"<h2><a href=https://luminosity-led.atlassian.net/browse/{story.key}>{story.key} </a>{story.get_field('summary')}</h2>" + '\n\n')
+            f.write(f"<h3><a href=https://luminosity-led.atlassian.net/browse/{story.key}>{story.key} </a>{story.get_field('summary')}</h3>" + '\n\n')
 
 if __name__ == '__main__':
-    write_version_fields()
+    write_version_description()
     write_bug_fields()
     write_story_fields()
