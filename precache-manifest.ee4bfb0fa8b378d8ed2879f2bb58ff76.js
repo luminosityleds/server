21911d9af09ff51d56352d4aa2eb5fa891c33ca9
pogi7 <pogi7@users.noreply.github.com>
2023-01-03 00:11:30 +0000

    deploy: b4f912eb8ebd514f247f56b4c66d291052b94db9

---
+++ precache-manifest.ee4bfb0fa8b378d8ed2879f2bb58ff76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0203940caf19126f0c39",
+    "url": "/static/css/main.9006ce91.chunk.css"
+  },
+  {
+    "revision": "0203940caf19126f0c39",
+    "url": "/static/js/main.0203940c.chunk.js"
+  },
+  {
+    "revision": "f12f5854ce980c3a420c",
+    "url": "/static/js/1.f12f5854.chunk.js"
+  },
+  {
+    "revision": "6477b55c8f25683a273a",
+    "url": "/static/js/2.6477b55c.chunk.js"
+  },
+  {
+    "revision": "86af42aa9a6f3e84c441",
+    "url": "/static/js/runtime~main.86af42aa.js"
+  },
+  {
+    "revision": "2f199fded91c86453e39a611d222c57d",
+    "url": "/index.html"
+  }
+];
