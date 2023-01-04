76e86ec9e9680e1d8bc98c92291e86a94d9cd48a
Verus20 <matthewlevitt20@gmail.com>
2023-01-03 18:55:17 -0800

    fixes LL-94: Remove username and password fields from sign in and sign up forms

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -5,41 +5,13 @@ import "../css/ThirdParty.css"
 import {Users} from "../Interfaces"
 import {Link} from 'react-router-dom'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
 import {AppleLogin, GoogleLogin, MicrosoftLogin, GitHubLogin} from "./ThirdParty";
 import { GoogleOAuthProvider } from "@react-oauth/google"
-
-const eye = <FontAwesomeIcon icon={faEye} />
-const eyeSlash = <FontAwesomeIcon icon={faEyeSlash} />
-
-function usePasswordVisibility(){
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(passwordVisible ? false : true);
-  }
-  
-
-  return(
-    <>
-      <input type={passwordVisible ? "text" : "password"} placeholder="Password" />
-      <i onClick={togglePasswordVisibility}>{passwordVisible ? eye : eyeSlash}</i>
-    </>
-  );
-}
-
-
 
 export const Login: FC<Users> = (props: Users) => {
   return (
     <div className="login-cover">
       <h1>Login</h1>
-      <input type="username" placeholder="Username" />
-      {usePasswordVisibility()}
-      
-      <div className="login-forgot-btns">
-        <div className="login-btn">Login</div>
-        <div className="forgot-pwd-btn">Forgot Password</div>
-      </div>
       {/* Start of third party login buttons */}
       <div className="third-party-btns">
         <div><AppleLogin /></div>
@@ -56,7 +28,7 @@ export const Login: FC<Users> = (props: Users) => {
         <div><GitHubLogin /></div>
       </div>
       {/* End of third party login buttons */}
-      <div className="sign-up-btn"><Link to="/register">Register</Link></div>
+      <div className="sign-up-btn"><Link to="/register">Register Here</Link></div>
     </div>
   );
 }
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -4,36 +4,13 @@ import "../css/App.css"
 import {Users} from "../Interfaces"
 import {Link} from 'react-router-dom'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEye} from "@fortawesome/free-solid-svg-icons";
-import {faEyeSlash} from "@fortawesome/free-solid-svg-icons";
 import { AppleRegister, GoogleRegister, MicrosoftRegister, GitHubRegister } from './ThirdParty';
 import { GoogleOAuthProvider } from "@react-oauth/google"
-
-const eye = <FontAwesomeIcon icon={faEye} />
-const eyeSlash = <FontAwesomeIcon icon={faEyeSlash} />
-
-function usePasswordVisibility(){
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(passwordVisible ? false : true);
-  }
-
-  return(
-    <>
-      <input type={passwordVisible ? "text" : "password"} placeholder="Password" />
-      <i onClick={togglePasswordVisibility}>{passwordVisible ? eye : eyeSlash}</i>
-    </>
-  );
-}
 
 export const Register: FC<Users> = (props: Users) => {
   return (
     <div className="register-cover">
         <h1>Register</h1>
-        <input type="username" placeholder="Username" />
-        {usePasswordVisibility()}
-
-        <div className="sign-up-btn">Sign Up</div>
         {/* Start of third party login buttons */}
       <div className="third-party-btns">
         <div><AppleRegister /></div>
