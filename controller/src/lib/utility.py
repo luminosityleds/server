70b3c1bc29a4707ee92ce5ff2153caf2abcf8826
aematei <alexanderematei@gmail.com>
2023-01-12 09:50:37 -0800

    Create lib/utility.py for various utility functions. Create an object for interface with the onboard LED

---
+++ controller/src/lib/utility.py
@@ -0,0 +1,3 @@
+from machine import Pin # type: ignore pylint: disable=E0401
+
+onboardLed = Pin("LED", Pin.OUT)
--- controller/src/main.py
+++ controller/src/main.py
@@ -1,5 +1,5 @@
 # main.py
 
 # Uncomment the following for an LED demo (connect WS2812's data line to GP4)
-# import lightDemo
-# lightDemo.run()
+from lib import lightDemo
+lightDemo.run()
