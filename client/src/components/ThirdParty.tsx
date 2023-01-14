c9b9af640286525d2f5b1f6871007c6f669e2714
Matthew <matthewlevitt20@gmail.com>
2023-01-13 20:42:06 -0700

    refs LL-95: Add azure/msal-browser package for Microsoft SSO

--- client/src/components/ThirdParty.tsx
+++ client/src/components/ThirdParty.tsx
@@ -3,8 +3,19 @@ import "../css/App.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faApple, faGoogle, faMicrosoft, faGithub} from "@fortawesome/free-brands-svg-icons";
 import { useGoogleLogin } from "@react-oauth/google"
+import { PublicClientApplication } from "@azure/msal-browser"
 import axios from "axios"
 
+// Microsoft config
+export const MicrosoftConfig = {
+  auth: {
+    // Place your clientId
+    clientId: '56d1f83f-16ae-432e-890c-31352348af92',
+    authority: 'https://login.microsoftonline.com/common/'
+  }
+}
+
+const msalInstance = new PublicClientApplication(MicrosoftConfig)
 
 // Login components
 export const AppleLogin = () => {
@@ -106,9 +117,24 @@ export const GoogleRegister = () => {
 };
 
 export const MicrosoftRegister = () => {
+  
+
+  const login = async function() {
+    var loginRequest = {
+      scopes: ["user.read"]
+    };
+  
+    try {
+      const loginResponse = await msalInstance.loginPopup(loginRequest);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
   <div>
-    <button className="third-party-btn">
+    <button className="third-party-btn" onClick={() => login()}>
       <FontAwesomeIcon className="third-party-icon" icon={faMicrosoft} size="2x" fixedWidth/>Register with Microsoft
     </button>
   </div>
