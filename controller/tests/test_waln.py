f115ed1c3816056d8a3bd8e89f597b256ce67299
aematei <alexanderematei@gmail.com>
2023-01-16 17:35:11 -0800

    Name pytest tests to implement

--- controller/tests/test_waln.py
+++ controller/tests/test_waln.py
@@ -1,4 +1,29 @@
 import pytest
+from controller.src.board import wlan, credentials
 
+def test_connect_success():
+    pass
 
+def test_connect_failure():
+    pass
 
+def test_connect_bad_arg():
+    pass
+
+def test_connect_already_connected():
+    pass
+
+def test_disconnect_connected():
+    pass
+
+def test_disconnect_non_connected():
+    pass
+
+def test_getCredentials():
+    pass
+
+def test_connected_true():
+    pass
+
+def test_connected_false():
+    pass
