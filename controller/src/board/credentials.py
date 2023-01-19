9e93f30cbbf2107aaf86cee963020f2abac01dd1
aematei <alexanderematei@gmail.com>
2023-01-19 13:37:46 -0800

    Create WLANCredentials class for handling wlan credentials

---
+++ controller/src/board/credentials.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class WLANCredentials():
+    """
+    Represents the credentials associated with a WLAN connection.
+    """
+    ssid: str = None
+    password: str = None
