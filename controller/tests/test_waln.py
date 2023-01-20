dfeb5cf8b2234a51cedf1f7f5d670f27ef43e8ed
aematei <alexanderematei@gmail.com>
2023-01-19 16:27:01 -0800

    Add wlan test for connection failure (ommiting wait codes)

--- controller/tests/test_waln.py
+++ controller/tests/test_waln.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 from controller.src.board import wlan, credentials
 
 CYW43_STATUS_CODES = {
+    "CYW43_LINK_BADAUTH": -3,
+    "CYW43_LINK_NONET": -2,
+    "CYW43_LINK_FAIL": -1,
     "CYW43_LINK_DOWN": 0,
     "CYW43_LINK_JOIN": 1,
     "CYW43_LINK_NOIP": 2,
-    "CYW43_LINK_UP": 3,
-    "CYW43_LINK_FAIL": -1,
-    "CYW43_LINK_NONET": -2,
-    "CYW43_LINK_BADAUTH": -3
+    "CYW43_LINK_UP": 3
 }
 
 @pytest.fixture
@@ -17,13 +17,15 @@ def wlan_credentials():
     return credentials.WLANCredentials("ssid", "pass")
 
 def test_connect_success(wlan_credentials):
-    wlan._wlan.status_code = wlan._CONNECTION_SUCCESS_CODE
+    wlan._wlan.status_code = CYW43_STATUS_CODES["CYW43_LINK_UP"]
     assert wlan_credentials == wlan.connect(wlan_credentials)
 
 def test_connect_failure(wlan_credentials):
-    wlan._wlan.status_code = 0
-    with pytest.raises(wlan.WLANConnectionError):
-        wlan.connect(wlan_credentials)
+    for key in ["CYW43_LINK_FAIL", "CYW43_LINK_NONET", "CYW43_LINK_BADAUTH"]:
+        wlan._wlan.status_code = key
+        with pytest.raises(wlan.WLANConnectionError):
+            wlan.connect(wlan_credentials)
+
 
 def test_connect_bad_arg():
     pass
