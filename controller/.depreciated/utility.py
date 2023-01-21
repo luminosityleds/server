806be8e490ebe2458a33d3c38b97c53e0b0f2cce
aematei <alexanderematei@gmail.com>
2023-01-20 19:00:53 -0800

    Remove depreciated controller/ modules

--- controller/.depreciated/utility.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin # type: ignore pylint: disable=E0401
-
-onboardLed = Pin("LED", Pin.OUT)
-
-def toggleOnboardLed():
-    if onboardLed.value() == 0:
-        onboardLed.on()
-    else:
-        onboardLed.off()
