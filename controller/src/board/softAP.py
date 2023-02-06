c3c0eb14b973a47a3b46610d0ea0e067f643b2d1
aematei <alexanderematei@gmail.com>
2023-02-05 17:30:51 -0800

    Copy and implement tinyweb for use as a server in the SoftAP commissioning process

--- controller/src/board/softAP.py
+++ controller/src/board/softAP.py
@@ -1,5 +1,11 @@
+"""
+A SoftAP implementation of device commissioning based on [Michael Horne's
+approach](https://www.recantha.co.uk/blog/?p=21398).
+"""
+
 import network # type: ignore pylint: disable=E0401
 from board.credentials import WLANCredentials
+import board.tinyweb as tinyweb
 
 _AP_ESSID = "LuminosityLEDs" # essid for the access point, when needed
 _AP_PASSWORD = "temporary_password" # password for the access point
@@ -8,9 +14,23 @@ def run() -> WLANCredentials:
     _ap = network.WLAN(network.AP_IF) # set WLAN to access point interface
     _ap.config(essid=_AP_ESSID, password=_AP_PASSWORD)
 
-    _ap.active(True)
+    _ap.active(True) # activate interface
 
     while _ap.active == False: # wait until AP is active
         pass
 
     print(_ap.ifconfig())
+
+    app = tinyweb.webserver()
+
+    # Serve a simple Hello World! response when / is called
+    # and turn the LED on/off using toggle()
+    @app.route('/')
+    async def index(request, response):
+        # Start HTTP response with content-type text/html
+        await response.start_html()
+        # Send actual HTML page
+        await response.send('<html><body><h1>Hello, world!</h1></body></html>\n')
+
+    # Run the web server as the sole process
+    app.run(host="0.0.0.0", port=80)
