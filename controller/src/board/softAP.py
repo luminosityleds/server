85fb8131fe0f038bc6fc4e0986c090274032b009
aematei <alexanderematei@gmail.com>
2023-02-05 17:04:06 -0800

    Move softAP code to run() method

--- controller/src/board/softAP.py
+++ controller/src/board/softAP.py
@@ -1,14 +1,16 @@
 import network # type: ignore pylint: disable=E0401
+from board.credentials import WLANCredentials
 
 _AP_ESSID = "LuminosityLEDs" # essid for the access point, when needed
 _AP_PASSWORD = "temporary_password" # password for the access point
 
-_ap = network.WLAN(network.AP_IF) # set WLAN to access point interface
-_ap.config(essid=_AP_ESSID, password=_AP_PASSWORD)
+def run() -> WLANCredentials:
+    _ap = network.WLAN(network.AP_IF) # set WLAN to access point interface
+    _ap.config(essid=_AP_ESSID, password=_AP_PASSWORD)
 
-_ap.active(True)
+    _ap.active(True)
 
-while _ap.active == False: # wait until AP is active
-    pass
+    while _ap.active == False: # wait until AP is active
+        pass
 
-print(_ap.ifconfig())
+    print(_ap.ifconfig())
