ac2e0a1ff2410c5143fa22765c0c3cef9fefd2c5
WhenitStarted <nteran13@gmail.com>
2023-02-10 01:56:57 -0800

    Team page CSS fixes

--- client/src/components/Team.tsx
+++ client/src/components/Team.tsx
@@ -1,58 +1,45 @@
 import React from "react";
-import {Link} from "react-router-dom"
+
 import "../css/App.css"
 
-const teamStyle = {
-  
-  padding: "10px 50px 20px",
-};
 
 export const Team = () => 
 {
   return (
 
-    
   <div className="team-page-cover">
-    <h1 style={{textAlign: "center",fontSize:60, padding: "20px", color: "#5FCFF4"}}> Team Page </h1>
-      {/* Start of third party login buttons */}
+    <h1 style={{textAlign: "center",fontSize:60, padding: "20px", color: "#67ACF7"}}> Team Page </h1>
       <h2 className="member-layout">
-        Aaron Levitt : Team Lead 
+            <h3>Aaron Levitt : Team Lead</h3>  
         </h2>
         <h2 className="member-layout">
-        Matthew Levitt : Full-stack Engineer 
+            <h3>Matthew Levitt : Full-stack Engineer </h3>
         </h2>
         <h2 className="member-layout">
-        Erika Ledesma : Full-stack Engineer 
+            <h3>Erika Ledesma : Full-stack Engineer </h3>
         </h2>  
-    <div className="team-page-cover"></div>
+    {/* <div className="team-page-cover"></div> */}
         <h2 className="member-layout">
-        Jason Chhan : Full-stack Engineer 
+            <h3> Jason Chhan : Full-stack Engineer </h3>
         </h2>
         <h2 className="member-layout">
-        Alan Mong : Full-stack Engineer 
+            <h3> Alan Mong : Full-stack Engineer </h3>
         </h2>
         <h2 className="member-layout">
-        Andrew Haigh : Cybersecurity Analyst 
+            <h3> Andrew Haigh : Cybersecurity Analyst </h3> 
         </h2>
-    <div className="team-page-cover"></div>
+    {/* <div className="team-page-cover"></div> */}
       <h2 className="member-layout">
-      Alex Matei : Embedded Software Engineer 
+            <h3> Alex Matei : Embedded Software Engineer </h3>
+            
       </h2>
       <h2 className="member-layout">
-      Nick Teran : Embedded Software Engineer 
+          <h3> Nick Teran : Embedded Software Engineer </h3>
       </h2>
-
-
     </div>
-
-
-
 
   );
 };
-
-
-
 
 export default Team;
 
