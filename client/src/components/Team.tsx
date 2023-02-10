8a3d3d9139afd320d9d45cc4296bcdca73a2497e
WhenitStarted <nteran13@gmail.com>
2023-02-10 00:02:45 -0800

    Adding team text to page

--- client/src/components/Team.tsx
+++ client/src/components/Team.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
-export const Team = () => {
+export const Team = () => 
+{
   return (
     <div className="team">
-        <h1>Team Page</h1>
+        <h1> Team Page </h1>
+        <p>
+    <div> Aaron Levitt : Team Lead </div>
+    <h2> Matthew Levitt : Full-stack Engineer </h2>
+    <h2> Erika Ledesma : Full-stack Engineer </h2>
+    <h2> Jason Chhan : Full-stack Engineer </h2>
+    <h2> Alan Mong : Full-stack Engineer </h2>
+    <h2> Andrew Haigh : Cybersecurity Analyst </h2>
+    <h2> Alex Matei : Embedded Software Engineer </h2>
+    <h2> Nick Teran : Embedded Software Engineer </h2>
+  </p>
+        
     </div>
   );
 };
+
+
 
 export default Team;
