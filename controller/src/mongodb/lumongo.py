aad10b7febce03521b86b54a2e07a112c8d9bf90
aematei <alexanderematei@gmail.com>
2023-04-06 10:53:05 -0700

    Add lumongo.py, with tests and updated workflow.

---
+++ controller/src/mongodb/lumongo.py
@@ -0,0 +1,82 @@
+# lumongo.py
+
+'''A simple library for querying the LL database.'''
+
+# Imports based on environment (dev or embedded)
+try: # to import uPy version of requests
+        import urequests as requests # type: ignore pylint: disable=E0401
+except ImportError: # import cPy version of requests
+        import requests # type: ignore pylint: disable=E0401
+
+try: # to import secrets from secrets.py
+        from src.secrets import MONGO_DAPI_KEY # type: ignore pylint: disable=E0401,E0611
+except ImportError: # import from environment instead (GH workflow)
+        try:
+                import os
+                MONGO_DAPI_KEY = f'{os.environ["MONGO_DAPI_KEY"]}'
+        except AttributeError:
+                pass
+
+# Constants
+BASE_URL = "https://data.mongodb-api.com/app/data-nobwt/endpoint/data/v1"
+API_KEY = MONGO_DAPI_KEY
+DATA_SOURCE = "LuminosityCluster-0"
+DATABASE = "Luminosity"
+COLLECTION = "devices"
+HEADERS = {
+    "api-key": f"{MONGO_DAPI_KEY}",
+    "Content-Type": "application/json"
+    }
+
+
+def findOne(filter : dict, projection : dict = None) -> dict:
+    body = findOneRequestBody(filter, projection)
+    response = requests.post(
+        BASE_URL + "/action/findOne", 
+        headers=HEADERS,
+        json=body
+        )
+
+    return response.json()["document"]
+
+def updateOne(filter : dict, update : dict) -> dict:
+    body = updateOneRequestBody(filter, update)
+    response = requests.post(
+        BASE_URL + "/action/updateOne", 
+        headers=HEADERS,
+        json=body
+        )
+
+    return response
+
+
+# Request body generators
+def findOneRequestBody(filter : dict, projection : dict = None) -> dict:
+    if projection is None:
+        body = {
+            "dataSource": DATA_SOURCE,
+            "database": DATABASE,
+            "collection": COLLECTION,
+            "filter": filter
+        }
+    else:
+        body = {
+            "dataSource": DATA_SOURCE,
+            "database": DATABASE,
+            "collection": COLLECTION,
+            "filter": filter,
+            "projection": projection
+        }
+
+    return body
+
+def updateOneRequestBody(filter : dict, update : dict) -> dict:
+    body = {
+        "dataSource": DATA_SOURCE,
+        "database": DATABASE,
+        "collection": COLLECTION,
+        "filter": filter,
+        "update": update
+    }
+
+    return body
---
+++ controller/test/mongodb/test_lumongo.py
@@ -0,0 +1,49 @@
+# test_lumongo.py
+
+import pytest 
+from src.mongodb.lumongo import findOneRequestBody,updateOneRequestBody, findOne, updateOne
+
+
+def test_findOneRequestBody_noProjection():
+    body = {
+        "dataSource": "LuminosityCluster-0",
+        "database": "Luminosity",
+        "collection": "devices",
+        "filter": {"uuid": "testtest"}
+    }
+    assert(findOneRequestBody({"uuid": "testtest"}) == body)
+
+def test_findOneRequestBody_WithProjection():
+    body = {
+        "dataSource": "LuminosityCluster-0",
+        "database": "Luminosity",
+        "collection": "devices",
+        "filter": {"uuid": "testtest"},
+        "projection": {"brightness": 1}
+    }
+    assert(findOneRequestBody({"uuid": "testtest"}, {"brightness": 1}) == body)
+
+def test_updateOneRequestBody():
+    body = {
+        "dataSource": "LuminosityCluster-0",
+        "database": "Luminosity",
+        "collection": "devices",
+        "filter": {"uuid": "testtest"},
+        "update": {"$set": {'brightness': 10}}
+                }
+    assert(updateOneRequestBody(
+        {"uuid": "testtest"},
+        {"$set": {'brightness': 10}}
+        ))
+
+def test_findOne_noProjection():
+    response = findOne({"uuid": "testtest"})
+
+def test_findOne_withProjection():
+    response = findOne({"uuid": "testtest"}, {"connected": 1})
+
+def test_updateOne():
+    originalValue = findOne({"uuid": "testtest"}, {"connected": 1})
+    # set the attribute to its original value to maintain state for other tests
+    response = updateOne({"uuid": "testtest"}, {"connected": originalValue})
+
