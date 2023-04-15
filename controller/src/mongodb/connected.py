6b5c5fb076e7af3d664e857286fdc306822db1eb
aematei <alexanderematei@gmail.com>
2023-04-14 19:22:50 -0700

    Refactor mongodb.connected to comply with uPy requirements

--- controller/src/mongodb/connected.py
+++ controller/src/mongodb/connected.py
@@ -1,37 +1,17 @@
-import os
-import pymongo
-from pymongo.collection import ReturnDocument
-import certifi
-import datetime
-
-# CA certificate needed to connect to MongoDB
-ca = certifi.where()
-
-# MongoDB Connection
-mongo = pymongo.MongoClient(f'{os.environ["MONGO_URL"]}',
-         tlsCAFile=ca)
-db = mongo.Luminosity
-collection = db.devices
+from . import lumongo
 
 def getConnected(uuid: str):
-        """ Getter for connected state in the DB.  Stdout when method is instantiated and the values."""
-        connected = (collection.find_one(filter={'uuid' : f"{uuid}"}))["connected"]
-        print(f"INFO | GET | CONNECTED: {connected} | TIME: {datetime.datetime.now()}")
-        return connected
+    """ Getter for connected state in the DB.  Stdout when method is instantiated and the values."""
+    response = lumongo.findOne({"uuid": uuid}, {"connected": 1})
+    connected = response.json()["document"]["connected"]
+    print(f"INFO | GET | CONNECTED: {connected}")
+    return connected
 
 def setConnected(uuid: str, value: bool):
-        """ Setter for connected state in the DB.  Stdout when method is instantiated and the values."""
-        try:
-                print(f"INFO | SET | CONNECTED: {value} | TIME: {datetime.datetime.now()}")
-                (collection.find_one_and_update(
-                        {'uuid' : f"{uuid}"},
-                        {'$set': {'connected': value, 
-                        'connectedTimestamp' : datetime.datetime.now(),
-                        'lastUpdated' : datetime.datetime.now()
-                                }
-                        },
-                        return_document = ReturnDocument.AFTER
-                ))
-        except Exception:
-                print(f"ERROR | SET | CONNECTED: {value} is not a valid connected value! | TIME: {datetime.datetime.now()}")
-                raise Exception
+    """ Setter for connected state in the DB.  Stdout when method is instantiated and the values."""
+    if isinstance(value, bool):
+        print(f"INFO | SET | CONNECTED: {value}")
+        response = lumongo.updateOne({"uuid": uuid}, {'$set': {'connected': value}})
+    else:
+        print(f"ERROR | SET | CONNECTED: {value} is not a valid connected value!")
+        raise Exception
