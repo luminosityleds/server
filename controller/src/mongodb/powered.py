2edff7e23d3ba13b7b793e08f66cbf9ff18bdbf2
aematei <alexanderematei@gmail.com>
2023-04-14 18:40:46 -0700

    Refactor mongodb.powered for compliance with uPy requirements

--- controller/src/mongodb/powered.py
+++ controller/src/mongodb/powered.py
@@ -1,37 +1,17 @@
-import os
-import pymongo
-from pymongo.collection import ReturnDocument
-import certifi
-import datetime
-
-# CA certificate needed to connect to MongoDB
-ca = certifi.where()
-
-# MongoDB Connection
-mongo = pymongo.MongoClient(f'{os.environ["MONGO_URL"]}',
-         tlsCAFile=ca)
-db = mongo.Luminosity
-collection = db.devices
+from . import lumongo
 
 def getPowered(uuid: str):
-        """ Getter for powered state in the DB.  Stdout when method is instantiated and the values."""
-        powered = (collection.find_one(filter={'uuid' : f"{uuid}"}))["powered"]
-        print(f"INFO | GET | POWERED: {powered} | TIME: {datetime.datetime.now()}")
-        return powered
+    """ Getter for powered state in the DB.  Stdout when method is instantiated and the values."""
+    response = lumongo.findOne({"uuid": uuid}, {"powered": 1})
+    powered = response.json()["document"]["powered"]
+    print(f"INFO | GET | POWERED: {powered}")
+    return powered
 
 def setPowered(uuid: str, value: bool):
-        """ Setter for powered state in the DB.  Stdout when method is instantiated and the values."""
-        try:
-                print(f"INFO | SET | POWERED: {value} | TIME: {datetime.datetime.now()}")
-                (collection.find_one_and_update(
-                        {'uuid' : f"{uuid}"},
-                        {'$set': {'powered': value, 
-                        'poweredTimestamp' : datetime.datetime.now(),
-                        'lastUpdated' : datetime.datetime.now()
-                                }
-                        },
-                        return_document = ReturnDocument.AFTER
-                ))
-        except Exception:
-                print(f"ERROR | SET | POWERED: {value} is not a valid powered value! | TIME: {datetime.datetime.now()}")
-                raise Exception
+    """ Setter for powered state in the DB.  Stdout when method is instantiated and the values."""
+    if isinstance(value, bool):
+        print(f"INFO | SET | POWERED: {value}")
+        response = lumongo.updateOne({"uuid": uuid}, {'$set': {'powered': value}})
+    else:
+        print(f"ERROR | SET | POWERED: {value} is not a valid powered value!")
+        raise Exception
