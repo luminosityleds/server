a3a39108d31ed58cd8de88daac1bf9029ce58da1
aematei <alexanderematei@gmail.com>
2023-04-29 23:22:37 -0700

    Add color state update function to main

--- controller/src/main.py
+++ controller/src/main.py
@@ -1,1 +1,12 @@
 # main.py
+
+# Demo for updating light state to db state
+from board import lights
+from board import mongodb
+
+def updateColorState():
+    dbColorStateHEX = mongodb.brightness.getBrightness()
+    dbColorStateRGB = tuple(int(dbColorStateHEX[i:i+2], 16) for i in (0, 2, 4))
+    if dbColorStateRGB != lights.getColor():
+        lights.setColor(dbColorStateRGB)
+
