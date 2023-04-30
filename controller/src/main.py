03edc9fb042ef15117d8083d1ae7287b8f81751e
aematei <alexanderematei@gmail.com>
2023-04-29 23:25:21 -0700

    Add brightness update function to main

--- controller/src/main.py
+++ controller/src/main.py
@@ -5,8 +5,12 @@ from board import lights
 from board import mongodb
 
 def updateColorState():
-    dbColorStateHEX = mongodb.brightness.getBrightness()
+    dbColorStateHEX = mongodb.color.getColor()
     dbColorStateRGB = tuple(int(dbColorStateHEX[i:i+2], 16) for i in (0, 2, 4))
     if dbColorStateRGB != lights.getColor():
         lights.setColor(dbColorStateRGB)
 
+def updateBrightnessState():
+    dbBrightnessState = mongodb.brightness.getBrightness()
+    if dbBrightnessState != lights.getBrightness():
+        lights.setBrightness(dbBrightnessState)
