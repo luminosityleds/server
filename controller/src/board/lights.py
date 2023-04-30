53be0a9e34d5dc68279cfb755e19c7d75333aac9
aematei <alexanderematei@gmail.com>
2023-04-29 22:57:42 -0700

    Refactor lights.py for stability and completeness

--- controller/src/board/lights.py
+++ controller/src/board/lights.py
@@ -1,90 +1,98 @@
-from machine import Pin  # pylint: disable=E0401
-import neopixel  # pylint: disable=E0401
-from time import sleep
+from neopixel import NeoPixel # type: ignore pylint: disable=E0401
+from machine import Pin # type: ignore pylint: disable=E0401
 
 # Constants
 _GPIO_PIN = 28
-_pixelCount = 60
-_OFFColor = [0, 0, 0]
-_ONColor = [255, 255, 255]
+_PIXEL_COUNT = 60
+_POWER_OFF_COLOR = (0, 0, 0)
 
 # Fields
-_pin = Pin(_GPIO_PIN, Pin.OUT)
-_neopixel = neopixel.NeoPixel(_pin, _pixelCount)
-_color = [0, 0, 0]
-_brightness = 0
+_pinOut = Pin(_GPIO_PIN, Pin.OUT)
+_color = (255, 255, 255)
+_brightness = 25
 _powered = False
-scaledColor = []
-UnscaledColor = []
+_neopixel = NeoPixel(_pinOut, _PIXEL_COUNT)
 
 # Public Methods
-def setColor(color:list):
-    '''
-    Initializes the color of the LED's
+def setColor(color : "tuple[int, int, int]"):
+    """
+    Sets the color of the LEDs
 
-    color (list): takes in the list variable for setting the color when instantiated.
-
-    '''
+    :param color: RGB representation of unit's color, 0-255 for each
+    """
+    print("here")
+    global _color
     _color = color
-    for i in range(_pixelCount):
-        _neopixel[i] = (_color)
-    _neopixel.write()
+    _updateUnit()
 
-def getColor() -> list:
-    '''
-    Returns list of the current color of LED's
+def getColor() -> "tuple[int, int, int]":
+    """
+    Returns current LED color value
+    """
+    return _color
 
-    '''
-    return (list(_neopixel[0]))
+def setBrightness(brightness : int):
+    """
+    Sets the brightness of the LEDs
 
-def setBrightness(_brightness:int):
-    '''
-    Allows to change brightness of LED's
+    :param brightness: the brightness of the unit, 0-100
+    """
+    global _brightness
+    _brightness = brightness
+    _updateUnit()
 
-    brightness (integer): takes in a anumerical value in order to change the brightness of the LED, scaled from 0 to 100
-
-    '''
-    
-    for component in getColor():
-        try:
-            scaledColor.append(int(round(component * _brightness / 100)))  
-        except ZeroDivisionError:
-            scaledColor.append(0)
-    setColor(scaledColor)
-
-
-def getBrightness()-> int:
-    '''
+def getBrightness() -> int:
+    """
     Returns integer of the current brightness of LED's
 
-    '''
-    for component in getColor():
-        try:
-            UnscaledColor.append(int(round((component * 100)/255)))  
-            
-        except ZeroDivisionError:
-            UnscaledColor.append(0)
-    _brightness= UnscaledColor[0] 
-    return (_brightness)
-        
+    :param brightness: the brightness of the unit, 0-100
+    """
+    return _brightness
 
+def setPowered(powered : bool):
+    """
+    Sets the powered state of the LEDs
 
-def setPowered(power:bool):
-    '''
-    Allows to set conencted state of LED's
-    '''
-    _powered = power
-    if _powered == True:
-        setColor(_ONColor)
-    if _powered == False:
-        setColor(_OFFColor)
-
+    :param powered: the powered state of the unit; True: on, False: off
+    """
+    global _powered
+    _powered = powered
+    _updateUnit()
 
 def getPowered() -> bool:
-    '''
-    Returns bool if LED's are on
+    """
+    Returns the powered state of the unit; True: on, False: off
 
-    '''
-    if list(_neopixel[0]) == _OFFColor:
-        return False
-    return True
+    """
+    return _powered
+
+#Private Methods
+def _scaleColorByBrightness() -> "tuple[int, int, int]":
+    """
+    Returns the unit's color tuple scaled to the unit's brightness.
+
+    :return: unit's RGB color tuple scaled by the unit's brightness
+    """
+    scaledColor = []
+
+    for component in _color:
+        try:
+            scaledColor.append(
+                int(round(component * _brightness / 100)))
+        except ZeroDivisionError:
+            scaledColor.append(0)
+
+    return tuple(scaledColor)
+
+def _updateUnit():
+    """
+    Updates the unit's brightness and color state based on object's
+    attributes.
+    """
+    if _powered:
+        _neopixel.fill(_scaleColorByBrightness())
+        _neopixel.write()
+
+    else: # turn all LEDs off
+        _neopixel.fill(_POWER_OFF_COLOR)
+        _neopixel.write()
