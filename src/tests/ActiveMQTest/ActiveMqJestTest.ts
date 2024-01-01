9dfc1874b20135a1386988414f9e7dc2af9541f4
Kevin Babakhani <kevinbabakhani@yahoo.com>
2024-01-01 20:06:05 +0000

    add 'npx ts-node' to pub/sub notify services
    Signed-off-by: pogi7 <aaronlevitt7@gmail.com>

---
+++ src/tests/ActiveMQTest/ActiveMqJestTest.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import server from '/workspaces/luminosityleds/src/tests/ActiveMQTest/publish.spec'; // Import your Express app
+
+describe('Publish Service', () => {
+  it('should return the expected response for /publish/:id', async () => {
+    const response = await request(server).get('/publish/3');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ id: '3', message: 'From Publish Service' });
+  });
+});
---
+++ src/tests/ActiveMQTest/jest.config.ts
@@ -0,0 +1,10 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+};
+
+export default config;
