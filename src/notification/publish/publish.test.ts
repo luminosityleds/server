33a5fae2558637831d92648fe04c195a0ed3069b
pogi7 <aaronlevitt7@gmail.com>
2024-01-02 00:20:19 -0800

    move publish.test.ts into publish folder
    Signed-off-by: pogi7 <aaronlevitt7@gmail.com>

--- src/notification/publish/publish.test.ts
+++ src/notification/publish/publish.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import server from '/workspaces/luminosityleds/src/notification/publish/publish'; // Import your Express app
+import server from './publish' // Import your Express app
 
 describe('Publish Service', () => {
   it('should return the expected response for /publish/:id', async () => {
